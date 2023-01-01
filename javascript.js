03309f1329a54c193168c57309a4b1f2ba2f70de
Johny <yonniedooo@gmail.com>
2023-01-02 02:44:51 +1100

    added basic buttons and display

--- javascript.js
+++ javascript.js
@@ -1,18 +1,13 @@
-function add(a, b) {
-    return a + b;
-} 
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
-function divide(a, b) {
-    return a / b;
-}
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
-    if (operator === add) return add(a, b);
+    if (operator === add) return add(a, b);  // do operator === + instead???? and same for the rest??
     else if (operator === subtract) return subtract(a, b);
     else if (operator === multiply) return multiply(a, b);
     else if (operator === divide) return divide(a, b);
