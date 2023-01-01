01a4080eb1258d231f9b2e699e921d17e80570b0
Johny <yonniedooo@gmail.com>
2023-01-01 21:12:17 +1100

    implement operate function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("hello")
+function add(a, b) {
+    return a + b;
+} 
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === add) return add(a, b);
+    else if (operator === subtract) return subtract(a, b);
+    else if (operator === multiply) return multiply(a, b);
+    else if (operator === divide) return divide(a, b);
+
+}
+
+console.log(operate(divide, 12, 3));
