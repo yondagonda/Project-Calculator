efb03cc14e949a8f39bac7150862bd0253051a48
Johny <yonniedooo@gmail.com>
2023-01-03 02:20:52 +1100

    implemented basic functionality with operators working

--- javascript.js
+++ javascript.js
@@ -7,11 +7,44 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
-    if (operator === add) return add(a, b);  // do operator === + instead???? and same for the rest??
-    else if (operator === subtract) return subtract(a, b);
-    else if (operator === multiply) return multiply(a, b);
-    else if (operator === divide) return divide(a, b);
-
+    if (operator === "add") return add(a, b);  // do operator === + instead???? and same for the rest??
+    else if (operator === "subtract") return subtract(a, b);
+    else if (operator === "multiply") return multiply(a, b);
+    else if (operator === "divide") return divide(a, b);
 }
 
-console.log(operate(divide, 12, 3));
+const numbers = document.querySelectorAll("#zero, #one, #two, #three, #four, #five, #six, #seven, #eight, #nine");
+const currentValue = document.getElementById("display")
+let displayValue = +currentValue.innerHTML
+
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        currentValue.innerHTML += e.target.innerHTML; 
+        let displayValue = +currentValue.innerHTML
+        console.log(displayValue);
+    })
+});
+
+const clears = document.querySelector("#clear")
+const operators = document.querySelectorAll("#add, #subtract, #multiply, #divide")
+const equals = document.querySelector("#equals")
+
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        let displayValue = currentValue.innerHTML
+        a = +displayValue;
+        currentValue.innerHTML = "";
+        chosenOperation = e.target.id;
+        console.log(chosenOperation);
+    })
+});
+
+equals.addEventListener('click', () => {
+    b = +currentValue.innerHTML;
+    console.log(operate(chosenOperation, a, b));
+    currentValue.innerHTML = operate(chosenOperation, a, b);
+})
+
+clears.addEventListener('click', () => {
+    return currentValue.innerHTML = "";
+});
