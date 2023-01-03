1a718addb5997ee5400edeaefcac9b12ad45a45a
Johny <yonniedooo@gmail.com>
2023-01-03 17:56:42 +1100

    general bug fixes like decimal rounding, division by 0, etc

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
-    if (operator === "add") return add(a, b);  // do operator === + instead???? and same for the rest??
+    if (operator === "add") return add(a, b);  
     else if (operator === "subtract") return subtract(a, b);
     else if (operator === "multiply") return multiply(a, b);
     else if (operator === "divide") return divide(a, b);
@@ -15,12 +15,11 @@ function operate(operator, a, b) {
 
 const numbers = document.querySelectorAll("#zero, #one, #two, #three, #four, #five, #six, #seven, #eight, #nine");
 const currentValue = document.getElementById("display")
-let displayValue = +currentValue.innerHTML
 
 numbers.forEach(number => {
     number.addEventListener('click', (e) => {
         currentValue.innerHTML += e.target.innerHTML; 
-        let displayValue = +currentValue.innerHTML
+        displayValue = +currentValue.innerHTML
         console.log(displayValue);
     })
 });
@@ -31,20 +30,25 @@ const equals = document.querySelector("#equals")
 
 operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
-        let displayValue = currentValue.innerHTML
-        a = +displayValue;
+        a = displayValue;
+
         currentValue.innerHTML = "";
         chosenOperation = e.target.id;
         console.log(chosenOperation);
+  
     })
 });
 
 equals.addEventListener('click', () => {
-    b = +currentValue.innerHTML;
+    b = displayValue
+
     console.log(operate(chosenOperation, a, b));
-    currentValue.innerHTML = operate(chosenOperation, a, b);
+    currentValue.innerHTML = Math.round(operate(chosenOperation, a, b) * 10000) / 10000; // ensures answers get rounded to max 4dp
+
+    if (b === 0 && chosenOperation === "divide") { // this conditional statement prevents division by 0
+        currentValue.innerHTML = " ";
+        return alert("nice try buddy");
+    }
 })
 
-clears.addEventListener('click', () => {
-    return currentValue.innerHTML = "";
-});
+clears.addEventListener('click', () => currentValue.innerHTML = "");
